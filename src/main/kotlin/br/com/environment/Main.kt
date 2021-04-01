aaa7433d6fee537acc5b8fdcb840bbc4749c83bd
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-01 10:41:40 -0300

    Comment instructions to build

--- src/main/kotlin/br/com/environment/Main.kt
+++ src/main/kotlin/br/com/environment/Main.kt
@@ -5,3 +5,10 @@ import tornadofx.launch
 fun main(args: Array<String>) {
     launch<Application>(args)
 }
+
+
+// https://www.jetbrains.com/help/idea/packaging-javafx-applications.html#troubleshoot
+
+// https://openjfx.io/openjfx-docs/#gradle
+
+// https://github.com/openjfx/samples/tree/master/HelloFX/Gradle
