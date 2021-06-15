9bce637520b52642ad40a52ba04dac9f677530d4
Tiago P. Casemiro <tiagocasemiro@pop-os.localdomain>
2021-06-15 18:02:22 -0300

    Adicionado comentatio

--- src/main/kotlin/br/com/environment/Main.kt
+++ src/main/kotlin/br/com/environment/Main.kt
@@ -6,6 +6,11 @@ fun main(args: Array<String>) {
     launch<Application>(args)
 }
 
+// muito importante para o bug da interacao com terminal https://stackoverflow.com/questions/29733038/running-interactive-shell-program-in-java
+
+
+//  olhar aqui
+// https://bitbucket.org/eloy_garcia_pca/wallpaperdownloader/src/master/
 
 // https://www.jetbrains.com/help/idea/packaging-javafx-applications.html#troubleshoot
 
